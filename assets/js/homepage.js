fd6377931356dc63b659a85c303bf0333736cfdd
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-06-05 13:44:16 -0500

    error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,9 +8,18 @@ var getUserRepos = function (user) {
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
   //make request to url
   fetch(apiUrl).then(function (response) {
-    response.json().then(function (data) {
-      displayRepos(data, user);
-    });
+    //response was successful
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayRepos(data, user);
+      });
+    } else {
+      alert("Error:GitHub user not found");
+    };
+  })
+  .catch(function (error) {
+    //notice this `catch()` getting chained onto the end of the `then()` method
+    alert("Unable to connect to gitHub");
   });
 };
 
@@ -27,6 +36,10 @@ var formSubmitHandler = function (event) {
 }
 
 var displayRepos = function (repos, searchTerm) {
+  if(repos.length ===0) {
+    repoContainerEl.textContent = "No repositories found";
+    return;
+  }
   //clear old content
   repoContainerEl.textContent = "";
   repoSearchTerm.textContent = searchTerm;
